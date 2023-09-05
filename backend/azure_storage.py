54839f5fac8180c1ec5357dc11eea7cd4476d254
EmmattxDev <84171888+EmmattxDev@users.noreply.github.com>
2023-09-05 22:14:52 +0100

    updated azure_storage file with the python-dotenv setup

--- backend/azure_storage.py
+++ backend/azure_storage.py
@@ -1,28 +1,19 @@
 import os
 from storages.backends.azure_storage import AzureStorage
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False),
-)
-
-
-# # Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR , '.env'))
+from dotenv import load_dotenv
+from backend.settings.base import BASE_DIR
+# load_dotenv()  # loads the configs from .env
+load_dotenv(BASE_DIR / '.env')
 
 class AzureMediaStorage(AzureStorage):
-    account_name = env('AZURE_ACCOUNT_NAME')
-    account_key = env('AZURE_ACCOUNT_KEY')
+    account_name = os.getenv('AZURE_ACCOUNT_NAME')
+    account_key = os.getenv('AZURE_ACCOUNT_KEY')
     azure_container = 'media'
     expiration_secs = None
 
 
 class AzureStaticStorage(AzureStorage):
-    account_name = env('AZURE_ACCOUNT_NAME')
-    account_key = env('AZURE_ACCOUNT_KEY')
+    account_name = os.getenv('AZURE_ACCOUNT_NAME')
+    account_key = os.getenv('AZURE_ACCOUNT_KEY')
     azure_container = 'static'
     expiration_secs = None
