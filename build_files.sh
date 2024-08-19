004952fbe20873de990499ce5e1f3f8f4208a647
EmmattxDev <84171888+EmmattxDev@users.noreply.github.com>
2024-08-19 20:24:26 +0100

    added some lines of code to build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # build_files.sh
 
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
